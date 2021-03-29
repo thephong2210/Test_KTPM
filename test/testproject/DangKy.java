5400ae211582e5549127581ba97f2dbac6e9f808
thephong2210 <thephong1412@gmail.com>
2021-03-29 11:37:53 +0700

    oki

--- test/testproject/DangKy.java
+++ test/testproject/DangKy.java
@@ -38,7 +38,7 @@ public class DangKy {
         // thiết lặp các đối tượng mặc định;
         
         System.out.println("Start");
-        System.setProperty("webdriver.chrome.driver","");
+        System.setProperty("webdriver.chrome.driver","test\\lib\\chromedriver.exe");
         driver=new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(20000, TimeUnit.MILLISECONDS);
         driver.get("http://localhost/web2general/checkout-registration.php");
@@ -55,16 +55,29 @@ public class DangKy {
       B4. Chờ thông báo.
       B5. Kiểm tra message.
      */
-    @Test
-     public void check_DangKy() throws InterruptedException {
-        
-        WebElement txt_username= driver.findElement(By.id("user_login"));
-    
-        txt_username.sendKeys("username_sai");
-        driver.findElement(By.name("user_password")).sendKeys("password");
-        driver.findElement(By.name("submit")).click();
-        WebElement lblerror= driver.findElement(By.className("alert-error"));
-        Assert.assertEquals(lblerror.getText(),"Login and/or password are wrong.");
+     @Test
+     // Xây dựng các testcae để check test 
+     /*
+      B1. Xác định control nhập dữ liệu
+      B2. Nhập dữ liệu vào username không tồn tại
+      B3. Click vào login
+      B4. Chờ thông báo.
+      B5. Kiểm tra message.
+     */
+     public void check_singupForm() throws InterruptedException {
+        //find username
+        WebElement txt_username= driver.findElement(By.id("firstname"));
+        // fill username in text username
+        txt_username.sendKeys("lamcubu2000");
+        driver.findElement(By.id("name")).sendKeys("mend1");
+        driver.findElement(By.id("email")).sendKeys("md11en@gmail.com");
+        driver.findElement(By.id("password")).sendKeys("ki111euoanh123");
+        driver.findElement(By.id("d-b-day")).sendKeys("11");
+        driver.findElement(By.id("d-b-month")).sendKeys("11");
+        driver.findElement(By.id("d-b-year")).sendKeys("2000");
+        driver.findElement(By.id("SubmitCreate")).click();
+//        WebElement lblerror= driver.findElement(By.className("alert-error"));
+//       assertEquals(lblerror.getText(),"Login and/or password are wrong.");
         // hoặc viết cách khác
         //driver.findElements(By.xpath("//input[@value='Sign in' and @type='submit']"));
         //WebElement btnsubmit = driver.findElement(By.xpath("//input[@type='submit' and @value='Sign in']"));
@@ -74,54 +87,54 @@ public class DangKy {
         //Thread.sleep(1000);
        
      }
-     @Test
-     public void checkcolor()
-     {
-        
-        String buttonColor = driver.findElement(By.name("submit")).getCssValue("background-color");
-        String buttonTextColor = driver.findElement(By.name("submit")).getCssValue("color");
-        System.out.println("Button color: " + buttonColor);
-        System.out.println("Text color " + buttonTextColor);
-     }
-    @BeforeMethod
-    //Chạy trước mỗi method test
-    // cài đặt reset để tránh làm nhiễu các testcase
-    public void setUpMethod() throws Exception {
-       driver.navigate().refresh();
-        
-    }
-    @Test
-    // kiểm tra đăng nhập thành công.
-    public void test2()
-    {
-        WebElement txt_username= driver.findElement(By.id("user_login"));
-        // fill username in text username
-        txt_username.sendKeys("username");
-        driver.findElement(By.name("user_password")).sendKeys("password");
-        driver.findElement(By.name("submit")).click();
-        driver.findElement(By.id("details-button")).click();
-        driver.findElement(By.id("proceed-link")).click();
-        
-        String expected_title="Zero Bank";
-        WebElement actual_title=driver.findElement(By.className("brand"));
-        Assert.assertEquals(expected_title,actual_title.getText());
-        //driver.findElement(By.linkText("Account Activity")).click();
-    }
-    @Test
-    // Kiểm tra độ rộng của cotrol
-    public void test3()
-    {   
-        driver.findElement(By.className("icon-user")).click();
-        driver.findElement(By.id("logout_link")).click();
-        driver.findElement(By.id("signin_button")).click();
-        WebElement txt_user= driver.findElement(By.id("user_login"));
-        int width_user =220;
-        int height_user=30;
-        System.out.println("Size sang:"+txt_user.getSize().width);
-        Assert.assertEquals(width_user, txt_user.getSize().width);
-        Assert.assertEquals(height_user, txt_user.getSize().height);
-      
-    }
+//     @Test
+//     public void checkcolor()
+//     {
+//        
+//        String buttonColor = driver.findElement(By.name("submit")).getCssValue("background-color");
+//        String buttonTextColor = driver.findElement(By.name("submit")).getCssValue("color");
+//        System.out.println("Button color: " + buttonColor);
+//        System.out.println("Text color " + buttonTextColor);
+//     }
+//    @BeforeMethod
+//    //Chạy trước mỗi method test
+//    // cài đặt reset để tránh làm nhiễu các testcase
+//    public void setUpMethod() throws Exception {
+//       driver.navigate().refresh();
+//        
+//    }
+//    @Test
+//    // kiểm tra đăng nhập thành công.
+//    public void test2()
+//    {
+//        WebElement txt_username= driver.findElement(By.id("user_login"));
+//        // fill username in text username
+//        txt_username.sendKeys("username");
+//        driver.findElement(By.name("user_password")).sendKeys("password");
+//        driver.findElement(By.name("submit")).click();
+//        driver.findElement(By.id("details-button")).click();
+//        driver.findElement(By.id("proceed-link")).click();
+//        
+//        String expected_title="Zero Bank";
+//        WebElement actual_title=driver.findElement(By.className("brand"));
+//        Assert.assertEquals(expected_title,actual_title.getText());
+//        //driver.findElement(By.linkText("Account Activity")).click();
+//    }
+//    @Test
+//    // Kiểm tra độ rộng của cotrol
+//    public void test3()
+//    {   
+//        driver.findElement(By.className("icon-user")).click();
+//        driver.findElement(By.id("logout_link")).click();
+//        driver.findElement(By.id("signin_button")).click();
+//        WebElement txt_user= driver.findElement(By.id("user_login"));
+//        int width_user =220;
+//        int height_user=30;
+//        System.out.println("Size sang:"+txt_user.getSize().width);
+//        Assert.assertEquals(width_user, txt_user.getSize().width);
+//        Assert.assertEquals(height_user, txt_user.getSize().height);
+//      
+//    }
     @AfterClass
      //Chạy trước tất cả method test trong class hiện tại
     public static void tearDownClass() throws Exception {
