5eec1b31b675251ae97b6e9d581e59654a124215
thephong2210 <thephong1412@gmail.com>
2021-03-31 21:19:13 +0700

    exel

--- test/testproject/DangKy.java
+++ test/testproject/DangKy.java
@@ -5,7 +5,6 @@ package testproject;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
@@ -21,75 +20,74 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
 /**
  *
  * @author admin
  */
 public class DangKy {
+
     static WebDriver driver;
-    String filepath="C:\\Users\\theph\\Desktop\\KTPM.xlsx";
+    String filepath = "exel\\TestcaseSignUp.xlsx";
+
     public DangKy() {
     }
-    ReadCellExample rc = new ReadCellExample(); 
-    
-        
+    ReadCellExample rc = new ReadCellExample();
 
-   
     @BeforeClass
     //Chạy trước method test đầu tiên trong class hiện tại
     public static void setUpClass() throws Exception {
         // thiết lặp các đối tượng mặc định;
-        
+
         System.out.println("Start");
-        System.setProperty("webdriver.chrome.driver","test\\lib\\chromedriver.exe");
-        driver=new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "test\\lib\\chromedriver.exe");
+        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(20000, TimeUnit.MILLISECONDS);
         driver.get("http://localhost/web2general/checkout-registration.php");
         //driver.manage().window().maximize();
         //driver.manage().window().setSize(new Dimension(800, 600));
         driver.manage().window().fullscreen();
     }
-     
-     // Xây dựng các testcae để check test 
-     /*
+
+    // Xây dựng các testcae để check test 
+    /*
       B1. Xác định control nhập dữ liệu
       B2. Nhập dữ liệu vào username không tồn tại
       B3. Click vào login
       B4. Chờ thông báo.
       B5. Kiểm tra message.
      */
-     @Test
-     // Xây dựng các testcae để check test 
-     /*
+    @Test
+    // Xây dựng các testcae để check test 
+    /*
       B1. Xác định control nhập dữ liệu
       B2. Nhập dữ liệu vào username không tồn tại
       B3. Click vào login
       B4. Chờ thông báo.
       B5. Kiểm tra message.
      */
-     public void check_singupForm() throws InterruptedException {
+    public void check_singupForm() throws InterruptedException {
         //find username
-        WebElement txt_username= driver.findElement(By.id("firstname"));
+        WebElement txt_username = driver.findElement(By.id("hoTen_input"));
         // fill username in text username
-        txt_username.sendKeys(rc.ReadCellData(2, 4, filepath));
-        driver.findElement(By.id("name")).sendKeys("sbdjbsdjbf");
-        driver.findElement(By.id("email")).sendKeys("mdsd111en@gmail.com");
-        driver.findElement(By.id("password")).sendKeys("kid1111euoanh123");
-        driver.findElement(By.id("d-b-day")).sendKeys("1");
+        txt_username.sendKeys(rc.ReadCellData(1, 0, filepath));
+//        driver.findElement(By.id("name")).sendKeys(rc.ReadCellData(2, 4, filepath));
+//        driver.findElement(By.id("email")).sendKeys(rc.ReadCellData(2, 4, filepath));
+//        driver.findElement(By.id("password")).sendKeys(rc.ReadCellData(2, 4, filepath));
+//        driver.findElement(By.id("d-b-day")).sendKeys("1");
         driver.findElement(By.id("d-b-month")).sendKeys("1");
         driver.findElement(By.id("d-b-year")).sendKeys("2000");
-        driver.findElement(By.id("SubmitCreate")).click();
+//        driver.findElement(By.id("SubmitCreate")).click();
 //        WebElement lblerror= driver.findElement(By.className("alert-error"));
 //       assertEquals(lblerror.getText(),"Login and/or password are wrong.");
         // hoặc viết cách khác
         //driver.findElements(By.xpath("//input[@value='Sign in' and @type='submit']"));
         //WebElement btnsubmit = driver.findElement(By.xpath("//input[@type='submit' and @value='Sign in']"));
         //System.out.println("duong dan:"+btnsubmit);
-        
+
         // chờ thông báo khi submit
         //Thread.sleep(1000);
-       
-     }
+    }
 //     @Test
 //     public void checkcolor()
 //     {
@@ -138,10 +136,11 @@ public class DangKy {
 //        Assert.assertEquals(height_user, txt_user.getSize().height);
 //      
 //    }
+
     @AfterClass
-     //Chạy trước tất cả method test trong class hiện tại
+    //Chạy trước tất cả method test trong class hiện tại
     public static void tearDownClass() throws Exception {
-       
+
     }
 
     @AfterMethod
@@ -151,6 +150,6 @@ public class DangKy {
         //giải phóng vùng nhớ sau khi xong. thường các kết nối đến database vì đối tượng đã được java giải phóng
         //driver.quit();
         System.out.println("End");
-        
+
     }
-} 
+}
