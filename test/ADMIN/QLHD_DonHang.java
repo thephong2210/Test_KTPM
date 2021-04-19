61500197bccc228e834c755f629b35a1d420b838
unknown <57187330+nguyenminhthien2000@users.noreply.github.com>
2021-04-19 10:02:23 +0700

    qlsp

--- test/ADMIN/QLHD_DonHang.java
+++ test/ADMIN/QLHD_DonHang.java
@@ -27,14 +27,146 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import static testproject.DangKy.driver;
 import static ADMIN.DangNhapAdmin.*;
+import java.util.HashSet;
 
 public class QLHD_DonHang {
 
     @Test
-    public static void check() throws InterruptedException, Exception {
-
+    public static void check_donhang() throws InterruptedException, Exception {
         ClickURL("http://localhost/web2general/admin/pages/orders.php");
+        //tìm kiếm theo mã đơn hàng
+        //khi không nhập ký tự
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/select")).sendKeys("Mã đơn hàng");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[1]")).sendKeys("");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[2]")).click();
+        Thread.sleep(3000);
+        
+        //tìm kiếm khi nhập thông tin nằm trong cơ sở dữ liệu
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/select")).sendKeys("Mã đơn hàng");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[1]")).sendKeys("19");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[2]")).click();
+        Thread.sleep(3000);
+        
+        //tìm kiếm khi nhập thông tin không nằm trong cở sở dữ liệu( hoặc tìm kiếm có nhập ký tự đặc biệt)
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[1]")).clear();
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/select")).sendKeys("Mã đơn hàng");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[1]")).sendKeys("20");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[2]")).click();
+        String errrorr = driver.switchTo().alert().getText() ;
+        Assert.assertEquals(errrorr,"Không tìm thấy dữ liệu!" );
+        Thread.sleep(2000); 
+        driver.switchTo().alert().accept();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[1]")).clear();
+        
+        //tìm kiếm theo mã khách hàng
+        //khi không nhập ký tự
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/select")).sendKeys("Mã khách hàng");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[1]")).sendKeys("");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[2]")).click();
+        Thread.sleep(3000);
+        
+        //tìm kiếm mã khách hàng khi nhập thông tin nằm trong cơ sở dữ liệu
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/select")).sendKeys("Mã khách hàng");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[1]")).sendKeys("9");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[2]")).click();
+        Thread.sleep(3000);
+        
+        //tìm kiếm khi nhập thông tin không nằm trong cở sở dữ liệu( hoặc tìm kiếm có nhập ký tự đặc biệt)
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[1]")).clear();
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/select")).sendKeys("Mã khách hàng");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[1]")).sendKeys("15");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[2]")).click();
+        Assert.assertEquals(errrorr,"Không tìm thấy dữ liệu!" );
+        Thread.sleep(2000); 
+        driver.switchTo().alert().accept();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[1]")).clear();
 
+        //tìm kiếm theo trạng thái
+        //khi không nhập ký tự
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/select")).sendKeys("Trạng thái");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[1]")).sendKeys("");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[2]")).click();
+        Thread.sleep(3000);
+        
+        //tìm kiếm trạng thái "Chưa giao"
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/select")).sendKeys("Trạng thái");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[1]")).sendKeys("Chưa giao");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[2]")).click();
+        Thread.sleep(3000);
+        
+        //tìm kiếm trạng thái "Đã hoàn thành"
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[1]")).clear();
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/select")).sendKeys("Trạng thái");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[1]")).sendKeys("đã hoàn thành");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[2]")).click();
+        Thread.sleep(3000);
+        
+        //tìm kiếm trạng thái khi nhập dữ liệu không có trong csdl
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[1]")).clear();
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/select")).sendKeys("Trạng thái");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[1]")).sendKeys("#$%%$%");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[2]")).click();
+        Assert.assertEquals(errrorr,"Không tìm thấy dữ liệu!" );
+        Thread.sleep(2000); 
+        driver.switchTo().alert().accept();
+        Thread.sleep(3000);
+        
+        //tìm kiếm theo ngày lập đơn hàng
+        //khi không nhập ký tự nào
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[1]")).clear();
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/select")).sendKeys("Ngày lập đơn hàng");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[1]")).sendKeys("");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[2]")).click();
+        Thread.sleep(3000);
+        
+        //khi nhập đúng định dạng ngày và nằm trong csdl
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/select")).sendKeys("Ngày lập đơn hàng");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[1]")).sendKeys("2020-11");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[2]")).click();
+        Thread.sleep(3000);
+        
+        //tìm kiếm sai định dạng hoặc những thứ không nằm trong cơ sở dữ liệu
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[1]")).clear();
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/select")).sendKeys("Ngày lập đơn hàng");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[1]")).sendKeys("2020-11-11-11-11");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[2]")).click();
+        Assert.assertEquals(errrorr,"Không tìm thấy dữ liệu!" );
+        Thread.sleep(2000); 
+        driver.switchTo().alert().accept();
+        Thread.sleep(3000);
+        
+        //click hiện tất cả
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/input[1]")).clear();
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[1]/a/input")).click();
+        
+        //click hiện lọc
+        //khi không nhập vào ngày tháng
+        driver.findElement(By.xpath("//*[@id=\"datepicker1\"]")).sendKeys("");
+        driver.findElement(By.xpath("//*[@id=\"datepicker2\"]")).sendKeys("");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[2]/input[3]")).click();
+        Thread.sleep(3000);
+        
+        //khi nhập đúng định dạng nằm trong csdl để trả về kết quả đúng
+        driver.findElement(By.xpath("//*[@id=\"datepicker1\"]")).sendKeys("2020-09-16");
+        driver.findElement(By.xpath("//*[@id=\"datepicker2\"]")).sendKeys("2020-12-02");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[2]/input[3]")).click();
+        Thread.sleep(3000);
+        
+        //khi nhập sai định trạng sẽ không trả về kết quả
+        driver.findElement(By.xpath("//*[@id=\"datepicker1\"]")).sendKeys("2020-09-16");
+        driver.findElement(By.xpath("//*[@id=\"datepicker2\"]")).sendKeys("2020-12-02");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div/div/div[2]/form[2]/input[3]")).click();
+        WebElement lblerror = driver.findElement(By.xpath("//*[@id=\"dataTables-example\"]/tbody/tr/td"));
+        Assert.assertEquals(lblerror.getText(), "No data available in table");
+        Thread.sleep(2000);
+        
+        
     }
-
+    @Test
+    public static void check_thongtin() throws InterruptedException, Exception {
+        driver.navigate().to("http://localhost/web2general/admin/pages/chitietdonhang.php?maDonHang=12");
+        //ClickURL("http://localhost/web2general/admin/pages/orders.php");
+    }
 }
--- test/ADMIN/QLSP_ThemSP.java
+++ test/ADMIN/QLSP_ThemSP.java
@@ -27,14 +27,132 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import static testproject.DangKy.driver;
 import static ADMIN.DangNhapAdmin.*;
+import static java.awt.PageAttributes.MediaType.C;
+import java.util.Set;
 
 public class QLSP_ThemSP {
 
+    // để trống tên sản phẩm khi thêm
     @Test
-    public static void check() throws InterruptedException, Exception {
-
+    public static void Add_Product() throws InterruptedException, Exception{
+        // để trống tên sản phẩm khi thêm
         ClickURL("http://localhost/web2general/admin/pages/productadd.php");
-
+        String errorr =  driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[1]/td[2]/input")).getAttribute("validationMessage");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/input")).click();
+        // lấy đoạn thông báo từ cái thằng input tên sản phẩm
+        Assert.assertEquals(errorr, "Please fill out this field.");
+        Thread.sleep(2000);
+        //setUpMethod();
+        
+        //để trống số lượng sản phẩm khi thêm
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[1]/td[2]/input")).sendKeys("Giầy");
+        String error =  driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[4]/td[2]/input")).getAttribute("validationMessage");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/input")).click();
+        // lấy đoạn thông báo từ cái thằng input số lượng
+        Assert.assertEquals(error, "Please fill out this field.");
+        Thread.sleep(2000);
+        setUpMethod();
+        
+        //để trống miêu tả sản phẩm khi thêm
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[1]/td[2]/input")).sendKeys("Giầy");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[4]/td[2]/input")).sendKeys("2");
+        String errorrr =  driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[5]/td[2]/textarea")).getAttribute("validationMessage");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/input")).click();
+        // lấy đoạn thông báo từ cái thằng input miêu tả
+        Assert.assertEquals(errorrr, "Please fill out this field.");
+        Thread.sleep(2000);
+        setUpMethod();
+        
+        //để trống giá sản phẩm khi thêm
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[1]/td[2]/input")).sendKeys("Giầy");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[4]/td[2]/input")).sendKeys("2");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[5]/td[2]/textarea")).sendKeys("nhẹ bền,...");
+        String errorrrr =  driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[6]/td[2]/input")).getAttribute("validationMessage");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/input")).click();
+        // lấy đoạn thông báo từ cái thằng input giá
+        Assert.assertEquals(errorrrr, "Please fill out this field.");
+        Thread.sleep(2000);
+        setUpMethod();
+        
+        //để trống hình ảnh sản phẩm khi thêm
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[1]/td[2]/input")).sendKeys("Giầy");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[4]/td[2]/input")).sendKeys("2");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[5]/td[2]/textarea")).sendKeys("nhẹ bền,...");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[6]/td[2]/input")).sendKeys("1500000");
+        String errorrrrr =  driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[7]/td[2]/input")).getAttribute("validationMessage");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/input")).click();
+        // lấy đoạn thông báo từ cái thằng input hình ảnh
+        Assert.assertEquals(errorrrrr, "Please select a file.");
+        Thread.sleep(2000);
+        setUpMethod();
+        
+        //không chọn danh mục 
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[1]/td[2]/input")).sendKeys("Giầy");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[4]/td[2]/input")).sendKeys("3");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[5]/td[2]/textarea")).sendKeys("nhe ben");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[6]/td[2]/input")).sendKeys("1500000");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[7]/td[2]/input")).sendKeys("C:/Users/Admin/Pictures/vietnam.png");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/input")).click();
+        String errrorr = driver.switchTo().alert().getText() ;
+        Assert.assertEquals(errrorr,"Chưa chọn danh mục sản phẩm!" );
+        Thread.sleep(2000); driver.switchTo().alert().accept();
+        setUpMethod();
+        
+        //không chọn size sản phẩm
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[1]/td[2]/input")).sendKeys("Giầy");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[4]/td[2]/input")).sendKeys("3");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[5]/td[2]/textarea")).sendKeys("nhe ben");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[6]/td[2]/input")).sendKeys("1500000");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[7]/td[2]/input")).sendKeys("C:/Users/Admin/Pictures/vietnam.png");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[2]/td[2]/select")).sendKeys("ADIDAS");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/input")).click();
+        String errrorrr = driver.switchTo().alert().getText() ;
+        Assert.assertEquals(errrorrr,"Chưa chọn size sản phẩm!" );
+        Thread.sleep(2000); driver.switchTo().alert().accept();
+        setUpMethod();
+        
+        //thêm sản phẩm thành công
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[1]/td[2]/input")).sendKeys("Giầy");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[4]/td[2]/input")).sendKeys("3");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[5]/td[2]/textarea")).sendKeys("nhe ben");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[6]/td[2]/input")).sendKeys("1500000");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[7]/td[2]/input")).sendKeys("C:/Users/Admin/Pictures/vietnam.png");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[2]/td[2]/select")).sendKeys("ADIDAS");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[3]/td[2]/select")).sendKeys("37");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/input")).click();
+        WebElement lblerror = driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/div"));
+        Assert.assertEquals(lblerror.getText(), "Thêm sản phẩm thành công!");
+        Thread.sleep(2000);
+        setUpMethod();
+        
+        
     }
-
+    @Test
+    public static void edit_Product() throws InterruptedException, Exception {
+        //sửa sản phẩm khi không nhập tên sản phẩm
+        driver.navigate().to("http://localhost/web2general/admin/pages/productedit.php?productid=2");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[1]/td[2]/input")).clear();
+        String error =  driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/table/tbody/tr[1]/td[2]/input")).getAttribute("validationMessage");
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/form/input")).click();
+        Assert.assertEquals(error, "Please fill out this field.");
+        Thread.sleep(2000);
+        setUpMethod();
+        
+    }
+//sai
+    //@Test
+    public static void Del_product()throws InterruptedException, Exception {
+        ClickURL("http://localhost/web2general/admin/pages/product.php?hideid=0");
+       // driver.navigate().to("http://localhost/web2general/admin/pages/product.php?hideid=0");
+        String errrorrr = driver.switchTo().alert().getText();
+        Assert.assertEquals(errrorrr,"Bạn có chắc muốn ẩn sản phẩm này không?" );
+        Thread.sleep(2000); driver.switchTo().alert().accept();
+        WebElement lblerror = driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/div[2]/div[1]"));
+        Assert.assertEquals(lblerror.getText(), "Ẩn sản phẩm thành công!");
+        Thread.sleep(2000);
+        setUpMethod();
+        
+    }
+    
 }
