7e6f5913c6bf41976d652049b68295ed7dbf5efa
Lâm <72560863+nthl2000@users.noreply.github.com>
2021-05-09 01:37:12 +0700

    Update DatHang.java

--- test/testproject/DatHang.java
+++ test/testproject/DatHang.java
@@ -19,57 +19,166 @@ public class DatHang {
 
     public DatHang() {
     }
-    int cartnum, selectednum, testcartnum, checkcartnum,price,amount,cash,totalcash;
-    String testname, testprice;
+    int cartnum, selectednum, testcartnum, testamount, checkcartnum, price, amount, cash, totalcash;
+    String testname, testprice, tfprice, tfcash, tfamount;
     String checkname, checkprice;
 
     @Test
     public static void check() throws InterruptedException, Exception {
-        LoginClientAndToPage("http://localhost:8080/web2general/");
+        LoginClientAndToPage("http://localhost/web2general/");
     }
 
     @Test
     public void checkDatHang() throws InterruptedException, Exception {
-        driver.navigate().to("http://localhost:8080/web2general/single-product.php?maSanPham=64");
+        driver.navigate().to("http://localhost/web2general/single-product.php?maSanPham=64");
         CheckChonHang();
-//        CheckChonHang_2();
+        CheckChonHang_2();
+        CheckChonHang_3();
+        checkCashCart();
+        driver.quit();
     }
 
     public void CheckChonHang() throws InterruptedException, Exception {
         cartnum = Integer.parseInt(driver.findElement(By.xpath("/html/body/header/div/div/div[1]/div/div/a/span")).getText());//lấy số trc khi bấm chọn,đang là 0
-        driver.navigate().to("http://localhost:8080/web2general/single-product.php?maSanPham=64");
-        testname= driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[1]/div[1]/div[2]/div/h2")).getText();
+        driver.navigate().to("http://localhost/web2general/single-product.php?maSanPham=64");
+        testname = driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[1]/div[1]/div[2]/div/h2")).getText();
         testprice = driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[1]/div[1]/div[2]/div/div[4]/h2")).getText();
         //Giá trị trên web mặc định là 0 nên phải nhập số lượng muốn chọn vào
         driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[1]/div[1]/div[2]/div/div[7]/form/div/div/div/div[2]")).click();
         driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[1]/div[1]/div[2]/div/div[7]/form/div/div/div/div[2]")).click();//Bấm + ==> 2
+//        testamount = Integer.parseInt(driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[1]/div[1]/div[2]/div/div[7]/form/div/div/div/input")).getAttribute("Value"));
         driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[1]/div[1]/div[2]/div/div[7]/form/input")).click();
         testcartnum = Integer.parseInt(driver.findElement(By.xpath("/html/body/header/div/div/div[1]/div/div/a/span")).getText());//lấy số sau khi bấm
         checkcartnum = cartnum + 3;
         Assert.assertEquals(testcartnum, checkcartnum);
-        driver.navigate().to("http://localhost:8080/web2general/cart.php");
+        driver.navigate().to("http://localhost/web2general/cart.php");
         checkname = driver.findElement(By.xpath("//*[@id=\"cart-summary\"]/tbody/tr/td[2]/p/a")).getText();
-        checkname=checkname.toUpperCase();
+        checkname = checkname.toUpperCase();
         checkprice = driver.findElement(By.xpath("//*[@id=\"cart-summary\"]/tbody/tr/td[4]/ul/li")).getText();
         System.out.println(testname);
         System.out.println(checkname);
         Assert.assertEquals(checkname, testname);
         Assert.assertEquals(checkprice, testprice);
-        price = Integer.parseInt(driver.findElement(By.xpath("//*[@id=\"cart-summary\"]/tbody/tr/td[4]/ul/li")).getText());
-        amount= Integer.parseInt(driver.findElement(By.xpath("//*[@id=\"cart-summary\"]/tbody/tr/td[5]/input")).getText());
-        cash= Integer.parseInt(driver.findElement(By.xpath("//*[@id=\"cart-summary\"]/tbody/tr/td[7]/span")).getText());
-//        Assert.assertEquals(price*amount, cash);
+        tfprice = driver.findElement(By.xpath("//*[@id=\"cart-summary\"]/tbody/tr/td[4]/ul/li")).getText();
+        tfprice = getNum(tfprice);
+//        System.out.println(tfprice);
+        price = Integer.parseInt(tfprice);
+//        System.out.println(price);
+        amount = Integer.parseInt(driver.findElement(By.xpath("//*[@id=\"cart-summary\"]/tbody/tr/td[5]/input")).getAttribute("Value"));
+        System.out.println(amount);
+        tfcash = driver.findElement(By.xpath("//*[@id=\"cart-summary\"]/tbody/tr/td[7]/span")).getText();
+        tfcash = getNum(tfcash);
+        cash = Integer.parseInt(tfcash);
+        Assert.assertEquals(price * amount, cash);
     }
 
     public void CheckChonHang_2() throws InterruptedException, Exception {
         cartnum = Integer.parseInt(driver.findElement(By.xpath("/html/body/header/div/div/div[1]/div/div/a/span")).getText());//lấy số trc khi bấm chọn,đang là 0
-        driver.navigate().to("http://localhost:8080/web2general/single-product.php?maSanPham=5");
+        driver.navigate().to("http://localhost/web2general/single-product.php?maSanPham=34");
+        testname = driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[1]/div[1]/div[2]/div/h2")).getText();
+        testprice = driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[1]/div[1]/div[2]/div/div[4]/h2")).getText();
         //Giá trị trên web mặc định là 0 nên phải nhập số lượng muốn chọn vào
+        driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[1]/div[1]/div[2]/div/div[7]/form/div/div/div/div[2]")).click();//Bấm + ==> 2
+        driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[1]/div[1]/div[2]/div/div[7]/form/input")).click();
+        testcartnum = Integer.parseInt(driver.findElement(By.xpath("/html/body/header/div/div/div[1]/div/div/a/span")).getText());//lấy số sau khi bấm
+        checkcartnum = cartnum + 2;
+        Assert.assertEquals(testcartnum, checkcartnum);
+        driver.navigate().to("http://localhost/web2general/cart.php");
+        checkname = driver.findElement(By.xpath("//*[@id=\"cart-summary\"]/tbody/tr/td[2]/p/a")).getText();
+        checkname = checkname.toUpperCase();
+        checkprice = driver.findElement(By.xpath("//*[@id=\"cart-summary\"]/tbody/tr/td[4]/ul/li")).getText();
+        System.out.println(testname);
+        System.out.println(checkname);
+        Assert.assertEquals(checkname, testname);
+        Assert.assertEquals(checkprice, testprice);
+        tfprice = driver.findElement(By.xpath("//*[@id=\"cart-summary\"]/tbody/tr/td[4]/ul/li")).getText();
+        tfprice = getNum(tfprice);
+//        System.out.println(tfprice);
+        price = Integer.parseInt(tfprice);
+//        System.out.println(price);
+        amount = Integer.parseInt(driver.findElement(By.xpath("//*[@id=\"cart-summary\"]/tbody/tr/td[5]/input")).getAttribute("Value"));
+        System.out.println(amount);
+        tfcash = driver.findElement(By.xpath("//*[@id=\"cart-summary\"]/tbody/tr/td[7]/span")).getText();
+        tfcash = getNum(tfcash);
+        cash = Integer.parseInt(tfcash);
+        Assert.assertEquals(price * amount, cash);
+    }
+
+    public void CheckChonHang_3() throws InterruptedException, Exception {
+        cartnum = Integer.parseInt(driver.findElement(By.xpath("/html/body/header/div/div/div[1]/div/div/a/span")).getText());//lấy số trc khi bấm chọn,đang là 0
+        driver.navigate().to("http://localhost/web2general/single-product.php?maSanPham=7");
+        testname = driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[1]/div[1]/div[2]/div/h2")).getText();
+        testprice = driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[1]/div[1]/div[2]/div/div[4]/h2")).getText();
+        //Giá trị trên web mặc định là 0 nên phải nhập số lượng muốn chọn vào
+        driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[1]/div[1]/div[2]/div/div[7]/form/div/div/div/div[2]")).click();
+        driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[1]/div[1]/div[2]/div/div[7]/form/div/div/div/div[2]")).click();
         driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[1]/div[1]/div[2]/div/div[7]/form/div/div/div/div[2]")).click();
         driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[1]/div[1]/div[2]/div/div[7]/form/div/div/div/div[2]")).click();//Bấm + ==> 2
         driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[1]/div[1]/div[2]/div/div[7]/form/input")).click();
         testcartnum = Integer.parseInt(driver.findElement(By.xpath("/html/body/header/div/div/div[1]/div/div/a/span")).getText());//lấy số sau khi bấm
-        checkcartnum = cartnum + 3;
+        checkcartnum = cartnum + 5;
         Assert.assertEquals(testcartnum, checkcartnum);
+        driver.navigate().to("http://localhost/web2general/cart.php");
+        checkname = driver.findElement(By.xpath("//*[@id=\"cart-summary\"]/tbody/tr/td[2]/p/a")).getText();
+        checkname = checkname.toUpperCase();
+        checkprice = driver.findElement(By.xpath("//*[@id=\"cart-summary\"]/tbody/tr/td[4]/ul/li")).getText();
+        System.out.println(testname);
+        System.out.println(checkname);
+        Assert.assertEquals(checkname, testname);
+        Assert.assertEquals(checkprice, testprice);
+        tfprice = driver.findElement(By.xpath("//*[@id=\"cart-summary\"]/tbody/tr/td[4]/ul/li")).getText();
+        tfprice = getNum(tfprice);
+//        System.out.println(tfprice);
+        price = Integer.parseInt(tfprice);
+//        System.out.println(price);
+        amount = Integer.parseInt(driver.findElement(By.xpath("//*[@id=\"cart-summary\"]/tbody/tr/td[5]/input")).getAttribute("Value"));
+        System.out.println(amount);
+        tfcash = driver.findElement(By.xpath("//*[@id=\"cart-summary\"]/tbody/tr/td[7]/span")).getText();
+        tfcash = getNum(tfcash);
+        cash = Integer.parseInt(tfcash);
+        Assert.assertEquals(price * amount, cash);
+
     }
+
+    public void checkCashCart() throws InterruptedException, Exception {
+        int cash1, cash2, cash3;
+        driver.navigate().to("http://localhost/web2general/cart.php");
+        //sp1
+        tfcash = driver.findElement(By.xpath("//*[@id=\"cart-summary\"]/tbody/tr[1]/td[7]/span")).getText();
+        tfcash = getNum(tfcash);
+        cash1 = Integer.parseInt(tfcash);
+        System.out.println(cash1);
+        //sp2
+        tfcash = driver.findElement(By.xpath("//*[@id=\"cart-summary\"]/tbody/tr[2]/td[7]/span")).getText();
+        tfcash = getNum(tfcash);
+        cash2 = Integer.parseInt(tfcash);
+        System.out.println(cash2);
+        //sp3
+        tfcash = driver.findElement(By.xpath("//*[@id=\"cart-summary\"]/tbody/tr[3]/td[7]/span")).getText();
+        tfcash = getNum(tfcash);
+        cash3 = Integer.parseInt(tfcash);
+        System.out.println(cash3);
+        //total cash all product
+        cash=cash1+cash2+cash3;
+        System.out.println(cash);
+        //total cash cart
+        tfcash = driver.findElement(By.xpath("//*[@id=\"total_product\"]")).getText();
+        tfcash = getNum(tfcash);
+        totalcash = Integer.parseInt(tfcash);
+        System.out.println(totalcash);
+        Assert.assertEquals(totalcash,cash);
+        Thread.sleep(1000);
+    }
+
+    public String removeChar(String str, char c) {
+        return str.replaceAll(Character.toString(c), "");
+    }
+
+    public String getNum(String str) {
+        str = str.replaceAll(",", "");
+        str = str.replaceAll("VNĐ", "");
+        str = str.trim();
+        return str;
+    }
+
 }
