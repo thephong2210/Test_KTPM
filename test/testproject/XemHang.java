37daa60e1e6041876f4dd5334ffb3db79cf1a6ac
Lâm <72560863+nthl2000@users.noreply.github.com>
2021-05-09 00:47:49 +0700

    Update XemHang.java

--- test/testproject/XemHang.java
+++ test/testproject/XemHang.java
@@ -19,6 +19,7 @@ public class XemHang {
     public XemHang(){}
     String testname,testprice;
     String checkname,checkprice;
+    String testbrandname,brandname;
     WebElement product;
     
     @Test
@@ -28,15 +29,14 @@ public class XemHang {
     @Test
     public void checkXemHang() throws InterruptedException, Exception{
         checkProduct();
-        Thread.sleep(1000);
-        driver.navigate().to("http://localhost/web2general/");
         checkProduct2();
-        Thread.sleep(1000);
-        driver.navigate().to("http://localhost/web2general/");
         checkProduct3();
-        Thread.sleep(1000);
-        driver.navigate().to("http://localhost/web2general/");
         checkProduct4();
+        //---------- Check brandname
+        checkBrand();
+        checkBrand2();
+        checkBrand3();
+        checkBrand4();
     }
     //Sản phẩm mới
     public void checkProduct()throws InterruptedException, Exception{
@@ -47,9 +47,11 @@ public class XemHang {
         checkprice = driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[1]/div[1]/div[2]/div/div[4]/h2")).getText();
         Assert.assertEquals(testname, checkname);
         Assert.assertEquals(testprice,checkprice); 
+        Thread.sleep(1000);
     }
     // Sản phẩm KM
     public void checkProduct2()throws InterruptedException, Exception{
+        driver.navigate().to("http://localhost/web2general/");
         testname = driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[2]/div/div[2]/div/div/div/div[1]/div/div[1]/div/div/div/div[2]/a")).getText();
         testprice = driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[2]/div/div[2]/div/div/div/div[1]/div/div[1]/div/div/div/div[2]/div[2]/span")).getText();
         driver.navigate().to("http://localhost/web2general/single-product.php?maSanPham=0");
@@ -57,9 +59,11 @@ public class XemHang {
         checkprice = driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[1]/div[1]/div[2]/div/div[4]/h2")).getText();
         Assert.assertEquals(testname, checkname);
         Assert.assertEquals(testprice,checkprice); 
+        Thread.sleep(1000);
     }
     //Sản phẩm nổi bật
     public void checkProduct3()throws InterruptedException, Exception{
+        driver.navigate().to("http://localhost/web2general/");
 //        driver.findElement(By.xpath("/html/body/section[2]/div/div[4]/div/div[2]/div/div/div[2]/div/div[1]/i")).click();//click mũi tên <-
         testname = driver.findElement(By.xpath("/html/body/section[2]/div/div[4]/div/div[2]/div/div/div[1]/div/div[4]/div/div/div[2]/a/span")).getText();
         testprice = driver.findElement(By.xpath("/html/body/section[2]/div/div[4]/div/div[2]/div/div/div[1]/div/div[4]/div/div/div[2]/div[2]/span")).getText();
@@ -68,16 +72,53 @@ public class XemHang {
         checkprice = driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[1]/div[1]/div[2]/div/div[4]/h2")).getText();
         Assert.assertEquals(testname, checkname);
         Assert.assertEquals(testprice,checkprice); 
+        Thread.sleep(1000);
     }
     //Sản phẩm Nike
     public void checkProduct4()throws InterruptedException, Exception{
+        driver.navigate().to("http://localhost/web2general/");
         testname = driver.findElement(By.xpath("/html/body/section[2]/div/div[6]/div[1]/div[2]/div/div/div[1]/div/div[4]/div/div/div[2]/a/span")).getText();
         testprice = driver.findElement(By.xpath("/html/body/section[2]/div/div[6]/div[1]/div[2]/div/div/div[1]/div/div[4]/div/div/div[2]/div[2]/span")).getText();
         driver.navigate().to("http://localhost/web2general/single-product.php?maSanPham=32");
         checkname = driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[1]/div[1]/div[2]/div/h2")).getText();
         checkprice = driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[1]/div[1]/div[2]/div/div[4]/h2")).getText();
         Assert.assertEquals(testname, checkname);
-        Assert.assertEquals(testprice,checkprice); 
+        Assert.assertEquals(testprice,checkprice);
+        Thread.sleep(1000);
+    }
+    //---------------------------------------------------------------------------
+    //Check BRANDNAME
+    public void checkBrand() throws InterruptedException, Exception{
+        driver.navigate().to("http://localhost/web2general/index.php");
+        brandname = driver.findElement(By.xpath("/html/body/header/div/div/div[2]/div/nav/ul/li[2]/a")).getText();
+        driver.navigate().to("http://localhost/web2general/shop-gird.php?maLoai=2");
+        testbrandname = driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[2]/div[1]/div[2]/h1/span[1]")).getText();
+        Assert.assertEquals(testbrandname,brandname); 
+        Thread.sleep(1000);
+    }
+    public void checkBrand2() throws InterruptedException, Exception{
+        driver.navigate().to("http://localhost/web2general/index.php");
+        brandname = driver.findElement(By.xpath("/html/body/header/div/div/div[2]/div/nav/ul/li[3]/a")).getText();
+        driver.navigate().to("http://localhost/web2general/shop-gird.php?maLoai=3");
+        testbrandname = driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[2]/div[1]/div[2]/h1/span[1]")).getText();
+        Assert.assertEquals(testbrandname,brandname); 
+        Thread.sleep(1000);
+    }
+    public void checkBrand3() throws InterruptedException, Exception{
+        driver.navigate().to("http://localhost/web2general/index.php");
+        brandname = driver.findElement(By.xpath("/html/body/header/div/div/div[2]/div/nav/ul/li[4]/a")).getText();
+        driver.navigate().to("http://localhost/web2general/shop-gird.php?maLoai=4");
+        testbrandname = driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[2]/div[1]/div[2]/h1/span[1]")).getText();
+        Assert.assertEquals(testbrandname,brandname); 
+        Thread.sleep(1000);
+    }
+    public void checkBrand4() throws InterruptedException, Exception{
+        driver.navigate().to("http://localhost/web2general/index.php");
+        brandname = driver.findElement(By.xpath("/html/body/header/div/div/div[2]/div/nav/ul/li[5]/a")).getText();
+        driver.navigate().to("http://localhost/web2general/shop-gird.php?maLoai=6");
+        testbrandname = driver.findElement(By.xpath("/html/body/section[2]/div/div[2]/div[2]/div[1]/div[2]/h1/span[1]")).getText();
+        Assert.assertEquals(testbrandname,brandname); 
+        Thread.sleep(1000);
     }
     
 }
